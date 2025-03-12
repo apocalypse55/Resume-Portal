848929971fdf063bf6655083b305c263c79300bc
apocalypse55 <shardool22rtipathi@gmail.com>
2025-03-12 19:45:30 +0545

    Spring Security authentication

---
+++ src/main/java/io/springboot/resume_portal/HomeController.java
@@ -0,0 +1,16 @@
+package io.springboot.resume_portal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello ";
+    }
+    @GetMapping("/edit")
+    public String edit() {
+        return "edit page ";
+    }
+}
